960cb671eade68ed9be6cac4ea9dbd041f7356a9
Ryan Quant <rquant@icloud.com>
2018-03-06 11:13:32 -0700

    Add custom error for short url

--- spec/models/short_url_spec.rb
+++ spec/models/short_url_spec.rb
@@ -10,7 +10,16 @@ describe ShortUrl, type: :model do
     it { is_expected.to validate_length_of(:slug).is_equal_to(5) }
     it { is_expected.to validate_presence_of :original_url }
     it { is_expected.to allow_value('http://foo.com').for(:original_url) }
-    it { is_expected.not_to allow_value('www.foo.com').for(:original_url) }
+
+    describe 'original url format is ivalid' do
+      let(:short_url) { build(:short_url, original_url: 'www.foo.com') }
+      before { short_url.valid? }
+
+      it 'has correct error' do
+        msg = I18n.t('activerecord.errors.models.short_url.attributes.original_url.invalid')
+        expect(short_url.errors.full_messages.last).to include("Original url #{msg}")
+      end
+    end
   end
 
   describe '#generate_slug' do
